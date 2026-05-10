b1eb72765b02e030da6a89aa9a96a1d482cbe362
maycotte <ho@maycotte.com>
2026-05-10 22:08:44 +0000

    chore: release v0.1.0

---
+++ packages/core/dist/apps/core/brand/index.js
@@ -0,0 +1,7 @@
+import { brandManifest } from './manifest.js';
+import { registerBrandRoutes } from './routes.js';
+export const brandApp = {
+    manifest: brandManifest,
+    registerRoutes: registerBrandRoutes,
+};
+//# sourceMappingURL=index.js.map
